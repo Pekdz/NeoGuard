03b39dc54021331effbf4d14ffe61db93e39d25b
ch <cheehau84@gmail.com>
2018-11-21 01:05:51 -0800

    dev

---
+++ app/src/main/java/ca/uwaterloo/crysp/privacyguard/Application/Activities/PacketDetails.java
@@ -0,0 +1,17 @@
+package ca.uwaterloo.crysp.privacyguard.Application.Activities;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import ca.uwaterloo.crysp.privacyguard.R;
+
+public class PacketDetails extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_packet_details);
+
+    }
+
+}

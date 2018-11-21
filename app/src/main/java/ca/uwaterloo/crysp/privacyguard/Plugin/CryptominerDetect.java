a5892723f86cdb7401e1220f6bb513ccc59416fe
Emanuela26 <xinchen.wang@helix.com>
2018-11-21 15:54:36 -0800

    add sms detector and crytpominer detector

---
+++ app/src/main/java/ca/uwaterloo/crysp/privacyguard/Plugin/CryptominerDetect.java
@@ -0,0 +1,61 @@
+package com.myapp.cryptominer;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+public class CryptominerDetect {
+    private String domain;
+    //private int portnumber;
+    private String ws_payload;
+    private List<String> urls = Arrays.asList("50btc.com", "abcpool.co", "alvarez.sfek.kz", "bitalo.com", "bitcoinpool.com",
+            "bitminter.com", "mmpool.bitparking.com", "blisterpool.com", "btcguild.com", "btcmine.com", "btcmp.com",
+            "btcmow.com", "btcwarp.com", "btcpoolman.com", "coinminers.co", "coinotron.com", "deepbit.net");
+    private Set<String> MiningPoolUrls = new HashSet<>(urls);
+    private static CryptominerDetect instance;
+    private CryptominerDetect() {
+
+    }
+    public static CryptominerDetect getInstance() {
+        if (instance == null)
+            instance = new CryptominerDetect();
+        return instance;
+    }
+
+    //detect url of mining pool
+    public void setAppDomain(String domain) {
+        this.domain = domain;
+    }
+    public boolean connectMiningPool() {
+        return MiningPoolUrls.contains(domain);
+    }
+
+    //check coinhive websocket payload pattern
+    public void setWsPayload(String payload) {
+        this.ws_payload = payload;
+    }
+    public boolean checkCoinHive() {
+        boolean hasBlob = false;
+        boolean hasTarget = false;
+        try {
+            JSONObject ws_obj = new JSONObject(ws_payload);
+            Iterator<String> keys = ws_obj.keys();
+            while(keys.hasNext()) {
+                String key = keys.next();
+                if(!hasBlob)    hasBlob = key.equals("blob");
+                if(!hasTarget) hasTarget = key.equals("target");
+                if(hasBlob && hasTarget)   return true;
+            }
+            return false;
+        } catch(JSONException e) {
+            // e.printStackTrace();
+            return false;
+        }
+    }
+}
---
+++ app/src/main/java/ca/uwaterloo/crysp/privacyguard/Plugin/SMSDetect.java
@@ -0,0 +1,38 @@
+package com.myapp.cryptominer;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SMSDetect {
+    private static final HashSet<String> smsList = new HashSet<>();
+    public void addSMSlist(String smsbody) {
+        smsList.add(smsbody);
+    }
+    private String generateCode(String Body) {
+        Pattern pattern = Pattern.compile("(\\d{4})");
+        Matcher matcher = pattern.matcher(Body);
+        String code = "";
+        if(matcher.find()) {
+            code = matcher.group(0);
+        }
+        return code;
+    }
+    public LeakReport handleRequest(String request) {
+        ArrayList<LeakInstance> leaks = new ArrayList<>();
+
+        for(String sms: smsList) {
+            String sms_code = generateCode(sms);
+            if (request.contains(sms_code)) {
+                leaks.add(new LeakInstance("Leak sms verification code", sms_code));
+            }
+        }
+        if(leaks.isEmpty()){
+            return null;
+        }
+        LeakReport rpt = new LeakReport(LeakReport.LeakCategory.CONTACT);
+        rpt.addLeaks(leaks);
+        return rpt;
+    }
+}
